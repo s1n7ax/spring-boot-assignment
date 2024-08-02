59cae6fc566deb2eee70cd4dbd69e873e42386df
s1n7ax <srineshnisala@gmail.com>
2024-08-02 12:57:08 +0530

    feat: add open API documentation

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-http POST localhost:8080/reader name="srinesh" email=srinesh@email.com
-http POST localhost:8080/reader name="nisala" email=nisala@email.com
-
-http PUT localhost:8080/book/12342341234 isbn=12342341234 title="title 1" author="user 1"
-http PUT localhost:8080/book/12342341234 isbn=12342341234 title="title 1" author="user 1"
-http PUT localhost:8080/book/12342341235 isbn=12342341235 title="title 2" author="user 2"
-http PUT localhost:8080/book/12342341235 isbn=12342341235 title="title 2" author="user 2"
